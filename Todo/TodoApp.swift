288929422cbcf68e2652a13edaeac5b2b926510c
kotathefriends <5070kouta@gmail.com>
2023-10-17 13:15:28 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by KOTA TAKAHASHI on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
